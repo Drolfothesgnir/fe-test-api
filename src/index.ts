ddf0e4126d426a38c530a750a7d375ab5768ad39
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2023-03-18 23:51:24 +0200

    added new fields to Product

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,29 @@ import jsonServer from "json-server";
 
 const port = process.env.PORT || 4000;
 const nProducts = process.env.COUNT || "20";
+
+const tagTypes = [
+  faker.commerce.department,
+  faker.commerce.product,
+  faker.commerce.productAdjective,
+  faker.commerce.productMaterial,
+];
+
+type CategorySet = { [key: string]: boolean };
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  color: string;
+  categories: CategorySet;
+  brand: string;
+  availability: boolean;
+  rating: number;
+}
+
 const db = {
   products: generateProducts(parseInt(nProducts)),
 };
@@ -18,17 +41,10 @@ server.get("/echo", (req, res) => {
 });
 
 server.use("/api/shop/", router);
+
 server.listen(port, () => {
   console.log("JSON Server is running");
 });
-
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  imageUrl: string;
-}
 
 function generateProducts(count: number): Product[] {
   const products: Product[] = [];
@@ -38,12 +54,64 @@ function generateProducts(count: number): Product[] {
       id: i + 1,
       name: faker.commerce.productName(),
       price: parseFloat(faker.commerce.price(10, 500)),
-      description: faker.lorem.sentence(),
-      imageUrl: faker.image.imageUrl(640, 480, "fashion", true),
+      description: faker.commerce.productDescription(),
+      imageUrl: faker.image.fashion(640, 480, true),
+      color: faker.color.human(),
+      categories: generateProductTags(),
+      brand: faker.company.name(),
+      availability: !!BernoulliDistribution(0.7),
+      rating: +(Math.random() * 5).toPrecision(2)
     };
 
     products.push(product);
   }
 
   return products;
+}
+
+function generateProductTags() {
+  const n = Math.floor(Math.random() * 6) + 1;
+  const result: CategorySet = {};
+  for (let i = 0; i < n; i++) {
+    const j = Math.floor(Math.random() * tagTypes.length);
+    const key = tagTypes[j]();
+    result[key] = true;
+  }
+  return result;
+}
+
+function discreteProbDist<T>(outcomes: T[], probabilities: number[]): T {
+  // Check that the arrays have the same length
+  if (outcomes.length !== probabilities.length) {
+    throw new Error("The arrays must have the same length");
+  }
+
+  // Calculate the cumulative probabilities
+  const cumulativeProbs: number[] = [];
+  let sum = 0;
+  for (let i = 0; i < probabilities.length; i++) {
+    sum += probabilities[i];
+    cumulativeProbs.push(sum);
+  }
+
+  // Generate a random number between 0 and 1
+  const rand = Math.random();
+
+  // Find the outcome that corresponds to the random number
+  for (let i = 0; i < cumulativeProbs.length; i++) {
+    if (rand < cumulativeProbs[i]) {
+      return outcomes[i];
+    }
+  }
+
+  // This should never happen
+  throw new Error("Error generating discrete probability distribution");
+}
+
+function BernoulliDistribution(p: number) {
+  if (p < 0 || p > 1) {
+    throw new Error("p must be between 0 and 1")
+  }
+
+  return discreteProbDist([1, 0], [p, 1 - p])
 }
