6df15525c7d5cf95cda3e3897aa91efb2e798a23
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2023-03-18 04:10:32 +0200

    fixed port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { faker } from '@faker-js/faker';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 interface Product {
   id: number;
